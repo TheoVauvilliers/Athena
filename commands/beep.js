9d5dbc256bce59470610fe3ddbf2e49ff3ddcc73
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-11-05 20:51:49 +0100

    Update beep command to respond with argument

--- commands/beep.js
+++ commands/beep.js
@@ -3,8 +3,13 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('beep')
-		.setDescription('Replies with boop!'),
+		.setDescription('Replies with boop!')
+		.addStringOption(option =>
+			option.setName('arg')
+				.setDescription('Your argument')
+				.setRequired(false)),
 	async execute(interaction) {
-		await interaction.reply('Boop!')
+		console.log(interaction.options)
+		await interaction.reply({ content: `Boop ${interaction.options.getString('arg')} !`, ephemeral: true })
 	},
 }
