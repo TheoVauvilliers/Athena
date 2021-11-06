b6b71acd0eeda74cc10e6398c079b0c963d18376
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-11-06 22:54:41 +0100

    refactor: use loadCommands in deploy-command and add todo for helpers.js

--- deploy-command.js
+++ deploy-command.js
@@ -3,13 +3,13 @@ const { SlashCommandBuilder } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 const { clientId, guildId, token } = require('./config.js')
+const { loadCommands } = require('./lib/helpers.js')
 
 // To get the names of commands in the commands directory using file names
 const commands = []
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
+const commandPath = loadCommands(fs)
+for (const path of commandPath) {
+	const command = require(path)
 	commands.push(command.data.toJSON())
 }
 
