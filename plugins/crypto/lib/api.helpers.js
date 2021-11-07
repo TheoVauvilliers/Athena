468e8313254e341c95f641e63b5feb16bd05892a
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-11-07 13:09:41 +0100

    docs: Add comments

--- plugins/crypto/lib/api.helpers.js
+++ plugins/crypto/lib/api.helpers.js
@@ -5,22 +5,26 @@ const { fetch } = require('cross-fetch')
 const baseApi = '//api.coincap.io/v2/'
 
 exports.getAvailableCrypto = async() => {
+    // Execute request
     const response = await(fetch(baseApi + 'assets?limit=5', {
         headers: {
             'Authorization': 'Bearer ' + coincapApiKey
         }
     }))
 
+    // If status is over 400, return error message
     if (response.status >= 400) {
         return 'An error has occurred, please contact support'
     }
 
+    // Transforme response in json
     const data = await response.json()
 
-    let name = []
+    let cryptoCurrNames = ''
+    // Loop on the json to get only the names of the crypto-currencies
     data.data.forEach(crypto => {
-        name.push(crypto.id)
+        cryptoCurrNames += `\n${crypto.id}`
     });
 
-    return name.join('\n')
+    return cryptoCurrNames
 }
