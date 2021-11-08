3476caf719fecb00c7561e97e926423d9ff26f39
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-11-08 20:47:54 +0100

    refactor: remove fs in index.js and

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const { Client, Collection, Intents } = require('discord.js')
 const { token } = require('./config.js')
 const { loadCommands } = require('./lib/helpers.js')
@@ -7,14 +6,14 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 // To get the names of commands in the commands directory using file names
 client.commands = new Collection()
-const commandPath = loadCommands(fs)
+const commandPath = loadCommands()
 for (const path of commandPath) {
 	const command = require(path)
 	client.commands.set(command.data.name, command)
 }
 
 client.once('ready', () => {
-	console.log('Thor is ready to break the world !')
+	console.log('Athena is ready !')
 })
 
 client.on('interactionCreate', async interaction => {
@@ -27,7 +26,6 @@ client.on('interactionCreate', async interaction => {
 	try {
 		await command.execute(interaction)
 	} catch (error) {
-		console.error(error)
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
 	}
 })
