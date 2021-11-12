af0f6b6b3cb634b8f30916b4cf902cacd7668181
Théo Vauvilliers <theo.vauvilliers10@gmail.com>
2021-11-12 08:50:09 +0100

    feat: add request fetchWalletById

--- plugins/crypto/commands/wallet.js
+++ plugins/crypto/commands/wallet.js
@@ -1,12 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { fetch, fetchOneById } = require('../../../lib/userRepository.js')
+const { fetch, fetchOneById, fetchWalletById } = require('../../../lib/userRepository.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('wallet')
 		.setDescription('WIP'),
 	async execute(interaction) {
-		console.log(fetchOneById(interaction.user.id))
+		console.log(fetchWalletById(interaction.user.id))
 		await interaction.reply({ content: 'WIP' })
 	},
 }
